c9e20df2a0870fcf20c92116bcaba5d74d67d29a
PariALLIANCE <kouamejoel468@gmail.com>
2025-12-31 19:11:03 +0000

    Update l.py

--- l.py
+++ l.py
@@ -16,8 +16,8 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 # === LIGUES ===
 LEAGUES = {
-    "Premier League": {"id": "eng.1", "json": "England_Premier_League.json"},
-    "LaLiga": {"id": "esp.1", "json": "Spain_Laliga.json"}
+    "Bundesliga": {"id": "ger.1", "json": "Germany_Bundesliga.json"},
+    "Argentina - Primera Nacional": {"id": "arg.2", "json": "Argentina_Primera_Nacional.json"}
 }
 
 BASE_URL = "https://www.espn.com/soccer/schedule/_/date/{date}/league/{league}"
